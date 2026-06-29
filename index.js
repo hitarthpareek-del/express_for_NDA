11c261fdadd89abc3f7e735735d672c7bc67142d
hitarthpareek-del <hitarth.pareek@mediccapress.in>
2026-06-29 12:15:27 +0530

    done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect("<YOUR_MONGODB_CONNECTION_STRING>")
+  .connect("mongodb+srv://hitarthpareek_db_user:rRIzINfIr6jDYrc1@leegality.i2zmexx.mongodb.net/?appName=leegality")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
