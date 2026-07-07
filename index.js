01dec90740e11c3ec386544f86065ed0f05d4644
hitarthpareek-del <hitarth.pareek@mediccapress.in>
2026-07-07 12:58:31 +0530

    test

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
-
-const leegalityRoutes = require("./routes/leegalityRoutes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.json({ message: "Backend is running" });
+  res.send("Working");
 });
 
-app.use("/api/leegality", leegalityRoutes);
-
-// Run locally only
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-
-  app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-  });
-}
-
-// Required for Vercel
 module.exports = app;
