b59365936ede798f7dec5229201cf0291ce7fda6
hitarthpareek-del <hitarth.pareek@mediccapress.in>
2026-07-07 12:59:53 +0530

    test1

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+const leegalityRoutes = require("./routes/leegalityRoutes");
+
+app.use("/api/leegality", leegalityRoutes);
+
 app.get("/", (req, res) => {
   res.send("Working");
 });
