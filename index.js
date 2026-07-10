bb03f52ab630daa22857c10dd3d7a47281aacec9
hitarthpareek-del <hitarth.pareek@mediccapress.in>
2026-07-10 14:02:22 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const leegalityRoutes = require("./routes/leegalityRoutes");
 
 app.use("/api/leegality", leegalityRoutes);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Working");
